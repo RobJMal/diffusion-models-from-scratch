c80443ea72094940611c8b8d10bf5293fab840b1
Robert Jomar Malate <robjmal@gmail.com>
2026-08-20 16:58:44 +0200

    feat: defined noise schedule and precomputed stuff

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ---- STEP 1 ----
 # Create 2D Swiss roll dataset
 N_SAMPLES = 2000
 theta = np.linspace(0, 3*np.pi, N_SAMPLES)
@@ -16,6 +17,22 @@ X = np.stack([x, y], axis=1)    # bundles the points
 X = (X - X.mean(axis=0)) / X.std(axis=0)
 X = torch.tensor(X, dtype=torch.float32)
 
-plt.scatter(X[:, 0], X[:, 1], s=5, alpha=0.5)
-plt.title("Target 2D distribution (Swiss Roll)")
-plt.show()
+# plt.scatter(X[:, 0], X[:, 1], s=5, alpha=0.5)
+# plt.title("Target 2D distribution (Swiss Roll)")
+# plt.show()
+
+# ---- STEP 2 ----
+# Define noise schedule pre-computations
+T = 100
+beta_start = 0.001
+beta_end = 0.2
+
+# Linear schedule for beta
+betas = torch.linspace(beta_start, beta_end, T)
+alphas = 1.0 - betas    # <T, 1>
+alpha_bars = torch.cumprod(alphas, dim=0)
+
+# Pre-calculate sqrts for close-form forward sampling equation
+sqrt_alpha_bars = torch.sqrt(alpha_bars)
+sqrt_one_minus_alpha_bars = torch.sqrt(1.0 - alpha_bars)
+
